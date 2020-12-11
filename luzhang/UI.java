77696d4851952b466fd9c32b7903f4280d227f44
luzhang04 <70920216+luzhang04@users.noreply.github.com>
2020-12-11 09:36:39 -0800

    Add files via upload

--- luzhang/UI.java
+++ luzhang/UI.java
@@ -1,16 +1,24 @@
+package ui.CheckLead;
+
+
+import dao.*;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.table.JTableHeader;
 import javax.swing.table.TableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+
 
 public class UI extends JFrame {
     private JPanel panel, tablePanel;
     private JTable table;
     private JLabel first, second;
-    private JButton button;
+    private JButton export, detail;
     private List<LeadForm> forms;
     private TableModel tableModel;
     private JComboBox filter, sortBy;
@@ -25,13 +33,14 @@ public class UI extends JFrame {
 
     private void addComponents() {
         FlowLayout f = new FlowLayout();
-        this.setLayout(f);
+        this.setLayout(new BorderLayout());
         panel = new JPanel();
         panel.add(first);
         panel.add(filter);
         panel.add(second);
         panel.add(sortBy);
-        panel.add(button);
+        panel.add(export);
+        panel.add(detail);
         this.add(panel);
 
         GridLayout g = new GridLayout(3,3);
@@ -40,9 +49,9 @@ public class UI extends JFrame {
         this.setLayout(g);
 
 
-        this.add(tablePanel);
+        this.add(tablePanel, BorderLayout.CENTER);
 
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
 
     }
@@ -56,32 +65,105 @@ public class UI extends JFrame {
     private void creat() {
         first = new JLabel("Filter");
         second = new JLabel("Sort by");
-        button = new JButton("Export");
+        export = new JButton("Export");
+        export.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (table.getSelectedRow() != -1) {
+                    int i = table.getSelectedRow();
+                    System.out.println(forms.get(i));
+                }
+            }
+        });
+        detail = new JButton("Detail");
+        detail.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (table.getSelectedRow() != -1) {
+                    int i = table.getSelectedRow();
+                    Vehicle[] vehicles = {new Vehicle("1", "2015", "Honda", null, true,
+                                "35000", "black", "black", BodyType.SUV, "0"),
+                                new Vehicle("2", "2020", "Toyota", null, false,
+                                        "25000", "black", "black", BodyType.CAR, "40000")};
+                    Address address = new Address("401 Terry Ave N #103", "","Seattle", "WA", "98109");
+                    Customer customer = new Customer("Xingfu", "Du", address, "(206) 467-5480", "Xingfu@gmail.com");
+                    String userNotes = "user notes here";
+                    new DetailsWindow(customer, vehicles, userNotes).buildGUI();
+                }
+            }
+        });
         String[] f = {"--Select filter--", "1", "2"};
         filter = new JComboBox(f);
-        filter.setSelectedIndex(0);
-        String[] s = {"--Select sort--", "1", "2"};
+        //filter.setSelectedIndex(0);
+        filter.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String s = (String) filter.getSelectedItem();
+                if (s == "car") {
+
+                }
+            }
+        });
+
+        String[] s = {"--Select sort--", "car", "model", "name", "phone", "email", "comments", "operation"};
         sortBy = new JComboBox(s);
-        sortBy.setSelectedIndex(0);
+        //sortBy.setSelectedIndex(0);
+        sortBy.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String s = (String) sortBy.getSelectedItem();
+                if (s == "car") {
+                    forms.sort(Comparator.comparing(a -> a.getCar()));
+                    table.updateUI();
+                }
+                if ( s == "model") {
+                    forms.sort(Comparator.comparing(a -> a.getModel()));
+                    table.updateUI();
+                }
+                if (s == "name") {
+                    forms.sort(Comparator.comparing(a -> a.getName()));
+                    table.updateUI();
+                }
+                if ( s == "phone") {
+                    forms.sort(Comparator.comparing(a -> a.getPhone()));
+                    table.updateUI();
+                }
+                if (s == "email") {
+                    forms.sort(Comparator.comparing(a -> a.getEmail()));
+                    table.updateUI();
+                }
+                if ( s == "comments") {
+                    forms.sort(Comparator.comparing(a -> a.getComments()));
+                    table.updateUI();
+                }
+            }
+        });
+
+
+
         creatTableComponents();
 
 
+
     }
-    private List<LeadForm> creatSomeForms() {
+    private List<LeadForm> createSomeForms() {
         ArrayList<LeadForm> forms = new ArrayList<LeadForm>();
-        forms.add(new LeadForm("BMW", "X1", "Joe", "1111111111",
+        forms.add(new LeadForm("Honda", "Y1", "Loe", "5111111111",
                 "111111@gmail.com", "I am interested in this car", "  "));
         forms.add(new LeadForm("BMW", "X2", "Moe", "2222211111",
                 "122111@gmail.com", "I am interested in this car", "  "));
-        forms.add(new LeadForm("BMW", "X3", "Loe", "3331111111",
+        forms.add(new LeadForm("Audi", "A3", "Joe", "6331111111",
                 "133111@gmail.com", "I am interested in this car", "  "));
-        forms.add(new LeadForm("BMW", "X4", "Yoe", "4411111111",
+        forms.add(new LeadForm("Ferrari", "F4", "Yoe", "4411111111",
                 "144111@gmail.com", "I am interested in this car", "  "));
+
         return forms;
     }
 
     private void creatTableComponents() {
-        forms = creatSomeForms();
+        forms = createSomeForms();
         tableModel = new LeadFormsTableModel(forms);
         table = new JTable(tableModel);
         tableFont = new Font("Baskerville", Font.PLAIN,18);
@@ -97,6 +179,9 @@ public class UI extends JFrame {
 
 
 
+
+
+
         table.setBackground(getContentPane().getBackground());
 
         jScrollPane = new JScrollPane(this.table);
@@ -107,7 +192,7 @@ public class UI extends JFrame {
         tablePanel = new JPanel();
         tablePanel.add(jScrollPane);
         tablePanel.setBackground(getContentPane().getBackground());
-        jScrollPane.setPreferredSize(new Dimension(1000, 420));
+        jScrollPane.setPreferredSize(new Dimension(1000, 600));
         jScrollPane.setBackground(getContentPane().getBackground());
     }
 
@@ -117,3 +202,10 @@ public class UI extends JFrame {
 
 
 }
+class Frame1 extends JFrame{
+    public Frame1(){
+        setSize(300,300);
+        setVisible(true);
+    }
+}
+
